d756ddb776f53397432d360998e3403246fb7067
Carmen Dukes <carmendukes@gmail.com>
2020-10-03 15:53:43 -0700

    update token verification

--- server.js
+++ server.js
@@ -14,8 +14,18 @@ app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
+/*
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
 
-
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
+})
+*/
 
 app.get('/movies', function handleGetMovies(req, res) {
     let response = movies.movies;
