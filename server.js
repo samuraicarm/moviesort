bac79439a7c2b6c9615c268e5ac94b727d195fd8
Carmen Dukes <carmendukes@gmail.com>
2020-10-03 15:29:07 -0700

    prod prep

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const helmet = require('helmet')
 const cors = require('cors')
 const movies = require('./movies.json')
 
-console.log(process.env.API_TOKEN)
+
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -61,8 +62,18 @@ app.get('/movies', function handleGetMovies(req, res) {
     res.json(response)
 })
 
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
+
 })
